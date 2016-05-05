bc5f10a82d96d67789c08a55eaf96dd2b3057385
Gabriel <gamatusevich@gmail.com>
2016-05-05 20:04:29 -0300

    added callback test

---
+++ __test__/callback-test.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var
+  chai = require('chai'),
+  expect = chai.expect,
+  _ = require('lodash'),
+  startCallback = require('../examples/callback/start-callback'),
+  stepCallback = require('../examples/callback/step-callback'),
+  endCallback = require('../examples/callback/end-callback'),
+  getMultiples = require('../examples/callback/get-multiples');
+
+
+describe('Callback Exercise', function () {
+
+  it('should return self after invocation', function () {
+
+    var
+      report = {
+        operations : 0,
+        steps : [],
+        result : []
+      },
+      start = startCallback.call(report),
+      steps = stepCallback.call(report),
+      end = endCallback.call(report);
+
+    expect(start).to.equal(report);
+    expect(steps).to.equal(report);
+    expect(end).to.equal(report);
+  });
+
+  it('should calculate if numbers are multiple and fill in the report', function () {
+    var
+      report = {
+        operations : 0,
+        steps : [],
+        result : []
+      },
+      numbers = [10, 45, 63, 120, 365, 9999],
+      multiple = 3,
+      step = 2,
+      expectedReports,
+      expectedResults;
+
+    getMultiples(numbers, multiple, step, startCallback.bind(report), stepCallback.bind(report), endCallback.bind(report));
+
+    expectedReports = [
+      '2 calculated',
+      '4 calculated',
+      '6 calculated'
+    ];
+
+    expectedResults = [
+      45, 63, 120, 9999
+    ];
+
+    expect(report.operations).to.eql(6);
+    expect(report.steps).to.eql(expectedReports);
+    expect(report.result).to.eql(expectedResults);
+
+  });
+
+});
